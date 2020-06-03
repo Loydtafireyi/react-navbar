0526f63a8f6cee57198744e7c315edba75d2863c
Loyd Tafireyi <loydtafireyi@gmail.com>
2020-06-03 14:01:25 +0200

    Home & About Pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
+import { Home } from './Home';
+import { About } from './About';
+import { NoMatch } from './NoMatch';
 
 function App() {
   return (
@@ -11,6 +14,11 @@ function App() {
       <React.Fragment>
         <Router>
           <NavigationBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route component={NoMatch} />
+          </Switch>
         </Router>
       </React.Fragment>
     </div>
